4f79d832c9a62c80bb45eb34045469c22439dfd5
jeffdstephens <jeffdstephens@gmail.com>
2023-02-17 17:26:44 -0500

    updated to v1

--- main.tf
+++ main.tf
@@ -137,7 +137,7 @@ resource "local_file" "kubeconfig" {
     - name: ${aws_eks_cluster.ms-up-running.arn}
       user:
         exec:
-            apiVersion: client.authentication.k8s.io/v1beta1
+            apiVersion: client.authentication.k8s.io/v1
             command: aws-iam-authenticator
             args:
               - "token"
